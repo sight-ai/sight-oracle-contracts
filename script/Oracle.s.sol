2b97d4e63df7302aa011ce878005662157675271
user00000001 <jaybrownjyb@gmail.com>
2024-08-01 18:33:45 +0800

    test: :white_check_mark: remove usecaseexample.
    Signed-off-by: user00000001 <jaybrownjyb@gmail.com>

--- script/Oracle.s.sol
+++ script/Oracle.s.sol
@@ -2,16 +2,14 @@
 pragma solidity ^0.8.13;
 
 import { Script, console } from "forge-std/Script.sol";
-import { UseCaseExample } from "../src/UseCaseExample.sol";
+import { Oracle } from "../contracts/Oracle/Oracle.sol";
 
-contract UseCaseExampleScript is Script {
+contract OracleScript is Script {
     function run() public {
-        address oracle = vm.envAddress("ORACLE_ADDR");
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
-        UseCaseExample uce = new UseCaseExample(address(oracle));
-        uce.singleRequest();
+        /* Oracle oracle =  */ new Oracle();
         vm.stopBroadcast();
     }
 }
